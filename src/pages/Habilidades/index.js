b17dbeab3c572ba379f5fcb0853856fb68c3add0
Mateusinhodev <mateusdev23@gmail.com>
2025-02-18 13:37:27 -0300

    Um toque de responsividade e seção de habilidades

--- src/pages/Habilidades/index.js
+++ src/pages/Habilidades/index.js
@@ -2,10 +2,34 @@ import React from "react";
 
 import "./style.css"
 
+function Titulo() {
+    return(
+        <div className="titulo-container">
+            <p className="titulo">SKILLS</p>
+        </div>
+    )
+}
+
 export default function Skills() {
     return(
-        <div>
-            <h1>Skills</h1>
+        <div className="skills-container">
+            <Titulo/>
+            <div className="skills-conteudo">
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/django/django-plain.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/firebase/firebase-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/babel/babel-original.svg" />
+            </div>
+
+
         </div>
     );
 }
