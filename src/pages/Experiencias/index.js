3f56f4b5d3ef41a713be9365756af9414f611ac5
Mateusinhodev <mateusdev23@gmail.com>
2025-03-13 13:53:41 -0300

    Seção Experiências concluida

--- src/pages/Experiencias/index.js
+++ src/pages/Experiencias/index.js
@@ -9,6 +9,11 @@ import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 import TimelineDot from '@mui/lab/TimelineDot';
+import WorkIcon from '@mui/icons-material/Work';
+import SchoolIcon from '@mui/icons-material/School';
+import CodeIcon from '@mui/icons-material/Code';
+import DisplaySettingsIcon from '@mui/icons-material/DisplaySettings';
+
 import FastfoodIcon from '@mui/icons-material/Fastfood';
 import LaptopMacIcon from '@mui/icons-material/LaptopMac';
 import HotelIcon from '@mui/icons-material/Hotel';
@@ -16,87 +21,101 @@ import RepeatIcon from '@mui/icons-material/Repeat';
 import Typography from '@mui/material/Typography';
 
 export default function Experiencias() {
+
     return(
         <div className="experiencia-container">
             <Titulo nome={"EXPERIÊNCIAS"}/>
             <div className="experiencia-conteudo">
-            <Timeline position="alternate">
-                <TimelineItem>
-                    <TimelineOppositeContent
-                    sx={{ m: 'auto 0' }}
-                    align="right"
-                    variant="body2"
-                    color="text.secondary"
-                    >
-                    9:30 am
-                    </TimelineOppositeContent>
-                    <TimelineSeparator>
-                    <TimelineConnector />
-                    <TimelineDot>
-                        <FastfoodIcon />
-                    </TimelineDot>
-                    <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent sx={{ py: '12px', px: 2 }}>
-                    <Typography variant="h6" component="span">
-                        Eat
-                    </Typography>
-                    <Typography>Because you need strength</Typography>
-                    </TimelineContent>
-                </TimelineItem>
-                <TimelineItem>
-                    <TimelineOppositeContent
-                    sx={{ m: 'auto 0' }}
-                    variant="body2"
-                    color="text.secondary"
-                    >
-                    10:00 am
-                    </TimelineOppositeContent>
-                    <TimelineSeparator>
-                    <TimelineConnector />
-                    <TimelineDot color="primary">
-                        <LaptopMacIcon />
-                    </TimelineDot>
-                    <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent sx={{ py: '12px', px: 2 }}>
-                    <Typography variant="h6" component="span">
-                        Code
-                    </Typography>
-                    <Typography>Because it&apos;s awesome!</Typography>
-                    </TimelineContent>
-                </TimelineItem>
-                <TimelineItem>
-                    <TimelineSeparator>
-                    <TimelineConnector />
-                    <TimelineDot color="primary" variant="outlined">
-                        <HotelIcon />
-                    </TimelineDot>
-                    <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
-                    </TimelineSeparator>
-                    <TimelineContent sx={{ py: '12px', px: 2 }}>
-                    <Typography variant="h6" component="span">
-                        Sleep
-                    </Typography>
-                    <Typography>Because you need rest</Typography>
-                    </TimelineContent>
-                </TimelineItem>
-                <TimelineItem>
-                    <TimelineSeparator>
-                    <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
-                    <TimelineDot color="secondary">
-                        <RepeatIcon />
-                    </TimelineDot>
-                    <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent sx={{ py: '12px', px: 2 }}>
-                    <Typography variant="h6" component="span">
-                        Repeat
-                    </Typography>
-                    <Typography>Because this is the life you love!</Typography>
-                    </TimelineContent>
-                </TimelineItem>
-                </Timeline>
+
+                <div className="experiencia-subtitulo">
+                    <p> <WorkIcon className="subtitulo-icone"/> Trabalho </p>
+                    <p> Estudo <SchoolIcon className="subtitulo-icone"/> </p>
+                </div>
+
+                <Timeline position="alternate">
+                    <TimelineItem>
+                        <TimelineOppositeContent
+                        sx={{ m: 'auto 0' }}
+                        align="right"
+                        variant="body2"
+                        color="whrite"
+                        >
+                        2021 - 2025
+                        </TimelineOppositeContent>
+                        <TimelineSeparator>
+                        <TimelineConnector />
+                        <TimelineDot>
+                            <CodeIcon />
+                        </TimelineDot>
+                        <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{ py: '12px', px: 2 }}>
+                        <Typography variant="h6" component="span">
+                            Ciências da Computação
+                        </Typography>
+                        <Typography>IFTO - Instituto Federal do Tocantins Campus Colinas</Typography>
+                        </TimelineContent>
+                    </TimelineItem>
+                    <TimelineItem>
+                        <TimelineOppositeContent
+                        sx={{ m: 'auto 0' }}
+                        variant="body2"
+                        color="whrite"
+                        >
+                        2022 - 2023
+                        </TimelineOppositeContent>
+                        <TimelineSeparator>
+                        <TimelineConnector />
+                        <TimelineDot>
+                            <DisplaySettingsIcon />
+                        </TimelineDot>
+                        <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{ py: '12px', px: 2 }}>
+                        <Typography variant="h6" component="span">
+                            Estágio em Tecnologia da Informação - TI
+                        </Typography>
+                        <Typography>Prefeitura Municipal de Guarai</Typography>
+                        </TimelineContent>
+                    </TimelineItem>
+                    <TimelineItem>
+                        <TimelineOppositeContent
+                        sx={{ m: 'auto 0' }}
+                        variant="body2"
+                        color="whrite"
+                        >
+                        2024 - Presente
+                        </TimelineOppositeContent>
+                        <TimelineSeparator>
+                        <TimelineConnector />
+                        <TimelineDot>
+                            <DisplaySettingsIcon />
+                        </TimelineDot>
+                        <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{ py: '12px', px: 2 }}>
+                        <Typography variant="h6" component="span">
+                            Técnico em Informática - TI
+                        </Typography>
+                        <Typography>Prefeitura Municipal de Guarai</Typography>
+                        </TimelineContent>
+                    </TimelineItem>
+                    {/* <TimelineItem>
+                        <TimelineSeparator>
+                        <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+                        <TimelineDot color="secondary">
+                            <RepeatIcon />
+                        </TimelineDot>
+                        <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{ py: '12px', px: 2 }}>
+                        <Typography variant="h6" component="span">
+                            Repeat
+                        </Typography>
+                        <Typography>Because this is the life you love!</Typography>
+                        </TimelineContent>
+                    </TimelineItem> */}
+                    </Timeline>
             </div>
         </div>
     )
